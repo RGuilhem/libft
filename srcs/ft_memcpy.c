0293909eff99097d981692c7c72450db307fb553
Guilhem Raux <g.raux@icloud.com>
2022-10-03 19:16:14 +0200

    added memcpy and memmove

--- srcs/ft_memcpy.c
+++ srcs/ft_memcpy.c
@@ -2,5 +2,17 @@
 
 void    *ft_memcpy(void *dest, const void *src, size_t n)
 {
+    unsigned int    i;
+    char            *dest_cpy;
+    char            *src_cpy;
+
+    i = 0;
+    dest_cpy = (char *) dest;
+    src_cpy = (char *) src;
+    while (i < n)
+    {
+        dest_cpy[i] = src_cpy[i];
+        i++;
+    }
     return (dest);
 }
