41ef38d5fb7537e703ebb7bce9d5572e4483a62a
Guilhem Raux <g.raux@icloud.com>
2022-10-09 20:13:40 +0200

    lstaddback

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,12 +6,12 @@
 /*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 15:47:57 by graux             #+#    #+#             */
-/*   Updated: 2022/10/07 16:03:04 by graux            ###   ########.fr       */
+/*   Updated: 2022/10/07 16:53:20 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-void	t_lstadd_back(t_list **lst, t_list *new_elem)
+void	ft_lstadd_back(t_list **lst, t_list *new_elem)
 {
 	t_list	*temp;
 
